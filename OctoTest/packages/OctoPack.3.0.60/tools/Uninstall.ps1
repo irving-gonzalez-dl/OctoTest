3daf5a34f65e2a07247bc89cfbed1d0f700c0861
Irving Gonzalez <i.gonzalez@wearedoublelime.com>
2018-06-29 13:08:55 -0500

    downgrade octopack

--- OctoTest/packages/OctoPack.3.0.60/tools/Uninstall.ps1
+++ OctoTest/packages/OctoPack.3.0.60/tools/Uninstall.ps1
@@ -13,31 +13,14 @@ param($installPath, $toolsPath, $package, $project)
   $itemsToRemove += $msbuild.Xml.Imports | Where-Object { $_.Project.EndsWith($package.Id + '.targets') }
   $itemsToRemove += $msbuild.Xml.Targets | Where-Object { $_.Name -eq "EnsureOctoPackImported" }
   
-  $saveProject = $false
-
   # Remove the elements and save the project
-  $saveProject = ($itemsToRemove -and $itemsToRemove.length)
-  foreach ($itemToRemove in $itemsToRemove)
+  if ($itemsToRemove -and $itemsToRemove.length)
   {
-	 $msbuild.Xml.RemoveChild($itemToRemove) | out-null
-  }
-
-  $targetImports = @()
-  $targetImports += $msbuild.Xml.Targets | Where-Object { $_.Name -eq "EnsureNuGetPackageBuildImports" }
-  foreach ($targetImport in $targetImports)
-  {
-	$targetImportsToRemove = @()
-	$targetImportsToRemove += $targetImports.Children | Where-Object { $_.Condition.Contains($package.Id + '.targets') }
-
-	$saveProject = $saveProject -or ($targetImportsToRemove -and $targetImportsToRemove.length)
-	foreach ($targetImport in $targetImportsToRemove)
-	{
-		$targetImports.RemoveChild($targetImport)
-	}
-  }
-
-  if ($saveProject)
-  {
+     foreach ($itemToRemove in $itemsToRemove)
+     {
+         $msbuild.Xml.RemoveChild($itemToRemove) | out-null
+     }
+     
      $isFSharpProject = ($project.Type -eq "F#")
      if ($isFSharpProject)
      {
